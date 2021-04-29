0b1131a6e01f88aab5f3a7d870fef8c95dacb049
MateuszTk <mateusz.tkaczewski@gmail.com>
2021-04-29 23:04:07 +0200

    Revert "OpenGL"
    This reverts commit 589af765db297c422d12d64e0f7b6e78ed208008.

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,7 +1,6 @@
 
+#include <SDL.h>
 #include "window.hpp"
-
-extern Window* window;
 
 /// Initialize LibTile2D
 bool lib_init() {
@@ -11,29 +10,4 @@ bool lib_init() {
 /// Exit LibTile2D
 void lib_exit() {
 	SDL_Quit();
-}
-
-void render() {
-	//Clear color buffer
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	//Bind program
-	glUseProgram(*window->getgProgramID());
-
-	//Enable vertex position
-	glEnableVertexAttribArray(*window->getgVertexPos2DLocation());
-
-	//Set vertex data
-	glBindBuffer(GL_ARRAY_BUFFER, *window->getgVBO());
-	glVertexAttribPointer(*window->getgVertexPos2DLocation(), 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
-
-	//Set index data and render
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *window->getgIBO());
-	glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
-
-	//Disable vertex position
-	glDisableVertexAttribArray(*window->getgVertexPos2DLocation());
-
-	//Unbind program
-	glUseProgram(NULL);
 }
